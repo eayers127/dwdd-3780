fa00ab23b1d73077388a0653f4112a6253fd97b2
Ethan Ayers <ethan.ayers88@gmail.com>
2024-04-04 16:28:39 -0600

    Make up class March 25. Connected to sales db

--- full-stack-sveltekit/src/routes/api/sales/+server.ts
+++ full-stack-sveltekit/src/routes/api/sales/+server.ts
@@ -1,15 +1,39 @@
 import { MongoClient, ObjectId } from 'mongodb';
+import clientPromise from '$lib/mongodb/mongodb.client';
+
 const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017'
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri);
+// const client = new MongoClient(uri);
 
 export async function GET(request: Request) {
     //const body = await request.json()
     //console.log(body)
-    let message = 'Hello World'
+    let client;
+    let salesData;
+
+    try {
+        client = await clientPromise
+        const salesDb = client?.db('sample_supplies');
+        const salesColl = salesDb?.collection('sales');
+
+        const salesArray = await salesColl?.find().toArray();
+
+        salesData = salesArray?.map(sale => {
+            return {...sale, _id: (sale._id as ObjectId).toString()}
+        })
+    } catch (error) {
+        console.error('Failed to connect to MongoDb sales');
+        if (client) {
+            await client.close();
+        }
+        return {
+            status: 500,
+            body: 'Failed to connect to MongoDb sales'
+        }
+    }
     return new Response(
-        JSON.stringify(message), {status: 200}
+        JSON.stringify(salesData), {status: 200}
     )
 }
 
