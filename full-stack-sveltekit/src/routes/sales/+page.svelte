71968256b6b6474c2ccc0e0a5218035e6bfc38cc
Ethan Ayers <ethan.ayers88@gmail.com>
2024-04-08 13:29:40 -0600

    Stats page styling updates

--- full-stack-sveltekit/src/routes/sales/+page.svelte
+++ full-stack-sveltekit/src/routes/sales/+page.svelte
@@ -71,23 +71,22 @@
 
 <section>
     <div class="flex flex-col justify-center items-center w-full h-screen">
-        <div class="flex flex-col justify-center items-center w-1/2 h-1/2 bg-slate-300 text-black">
-            <h2>Sales Data</h2>
-            <p>Show data here</p>
+        <div class="flex flex-col justify-center items-center w-1/2 h-1/2 bg-slate-300 text-black rounded-md">
+            <h2 class="font-bold text-2xl">Sales Data</h2>
             <button class="btn variant-outline-secondary m-2" on:click={getSalesData}>Get Sales Data</button>
             <button class="btn variant-outline-secondary" on:click={postSalesData}>Post Sales Data</button>
         </div>
-        <div class="flex bg-slate-600">
+        <div class="flex bg-slate-600 mt-4 rounded-md">
             <ListBox class="w-64">
                 {#each uniqueStoreLocations as location}
                     <ListBoxItem class="text-center" bind:group={singleLocation} name="medium" value={location} on:click={handleLocationSelection}>{location}</ListBoxItem>
                 {/each}
             </ListBox>
             <div class="p-4 m-2">
-                <h2>Sales Stats for {singleLocation}</h2>
-                <p>Total Items Sold: {salesStats.totalItemsSold}</p>
-                <p>Total Sales: {salesStats.totalSales}</p>
-                <p>Average Sales: {salesStats.averageSales}</p>
+                <h2><span class="font-semibold text-xl">Sales Stats for:</span> {singleLocation}</h2>
+                <p><span class="font-semibold">Total Items Sold:</span> {salesStats.totalItemsSold}</p>
+                <p><span class="font-semibold">Total Sales:</span> {salesStats.totalSales}</p>
+                <p><span class="font-semibold">Average Sales:</span> {salesStats.averageSales}</p>
                 <button class="btn variant-filled-primary" disabled={!singleLocation} on:click={exportSalesData}>Export Sales Data</button>
             </div>
         </div>
