9f95b1c4f1f6ea0ba8ba183f040b92811f17fc7e
Ethan Ayers <ethan.ayers88@gmail.com>
2024-04-08 14:15:21 -0600

    Class April 3. Added chart for sales data

--- full-stack-sveltekit/src/routes/sales/+page.svelte
+++ full-stack-sveltekit/src/routes/sales/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
-    import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton'
+    import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
+    import Chart from 'chart.js/auto'
+    import { onMount } from 'svelte';
 
     let allSalesData: any = []
     $: uniqueStoreLocations = [...new Set(allSalesData.map((sale: any) => sale.storeLocation))];
@@ -12,29 +14,48 @@
 
     let singleLocation: string = '';
 
+    let canvasElement: HTMLCanvasElement;
+    let ctx: CanvasRenderingContext2D | null;
+    // ctx is common syntax to represent 'context'
+
+    let isChartVisible = false;
+
+    onMount(() => {
+        ctx = canvasElement.getContext('2d');
+    })
+
+    function graphSalesData() {
+        if(ctx && allSalesData.length > 0) {
+            new Chart(ctx, {
+                type: 'bar',
+                data: {
+                labels: uniqueStoreLocations,
+                datasets: [{
+                    label: '# of Sales',
+                    data: uniqueStoreLocations.map((location: any) => totalSalesByLocation(location)),
+                    borderWidth: 1,
+                    backgroundColor: 'rgba(0, 0, 255, 0.4)'
+                }]
+                },
+                options: {
+                scales: {
+                    y: {
+                    beginAtZero: true
+                    }
+                }
+                }
+            });
+        }
+    };
+
+    function totalSalesByLocation(location: string) {
+        return allSalesData.filter((sale: any) => sale.storeLocation === location).length
+    }
+
     async function getSalesData() {
         const response = await fetch('/api/sales');
         const data = await response.json();
         allSalesData = data;
-    };
-
-    async function postSalesData() {
-        // const response = await fetch('/api/sales', {
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json'
-        //     },
-        //     body: JSON.stringify([{
-        //         storeLocation: 'Denver',
-        //         totalSales: 1000
-        //     },
-        //     {
-        //         storeLocation: 'San Diego',
-        //         totalSales: 2000
-        //     }])
-        // });
-        // const data = await response.json();
-        // console.log(data)
     };
 
     function handleLocationSelection(event: any) {
@@ -74,9 +95,12 @@
         <div class="flex flex-col justify-center items-center w-1/2 h-1/2 bg-slate-300 text-black rounded-md">
             <h2 class="font-bold text-2xl">Sales Data</h2>
             <button class="btn variant-outline-secondary m-2" on:click={getSalesData}>Get Sales Data</button>
-            <button class="btn variant-outline-secondary" on:click={postSalesData}>Post Sales Data</button>
+            <button class="btn variant-outline-secondary" disabled={allSalesData.length === 0} on:click={graphSalesData}>Graph Sales Data</button>
         </div>
         <div class="flex bg-slate-600 mt-4 rounded-md">
+            <div class="w-1/2">
+                    <canvas class="bg-slate-100 rounded-md m-2" bind:this={canvasElement}></canvas>
+            </div>
             <ListBox class="w-64">
                 {#each uniqueStoreLocations as location}
                     <ListBoxItem class="text-center" bind:group={singleLocation} name="medium" value={location} on:click={handleLocationSelection}>{location}</ListBoxItem>
