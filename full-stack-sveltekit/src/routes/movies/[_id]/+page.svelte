7062881c4201c6c5e57cb6c41e957d2e41302dcf
Ethan Ayers <ethan.ayers88@gmail.com>
2024-04-23 20:41:19 -0600

    Update individual movie page styling

--- full-stack-sveltekit/src/routes/movies/[_id]/+page.svelte
+++ full-stack-sveltekit/src/routes/movies/[_id]/+page.svelte
@@ -4,20 +4,22 @@
 
 </script>
 
-<main>
-    <div class="card max-w-md mx-auto bg-white overflow-hidden md:max-w-2xl m-4">
+<main class="p-4">
+    <div class="max-w-md mx-auto overflow-hidden md:max-w-2xl m-4 bg-slate-200 rounded-md">
         <div class="md:flex">
-            <h1>{data?.body?.movie?.title}</h1>
-        </div>
-        <div>
+            <div class="p-8 w-72">
+                <h1 class="text-gray-700 text-2xl font-bold mb-4">{data?.body?.movie?.title}</h1>
+                <p class="text-gray-600">{data?.body?.movie?.year}</p>
+                <p class="text-gray-600 mb-4">{data?.body?.movie?.rated} | {data?.body?.movie?.runtime} minutes</p>
+            </div>
             <div class="md:flex-shrink-0">
-                <img class="p-4 h-72 w-full object-cover md:w-48 rounded-xl" src={data?.body?.movie?.poster} 
-                alt={data?.body?.movie?.title}/>
+                <img class="p-4 h-72 w-full object-cover md:w-48 rounded-xl" src={data?.body?.movie?.poster} alt={data?.body?.movie?.title}/>
             </div>
         </div>
         <div class="p-8">
-            {data?.body?.movie?.year}
-            <p class="text-ellipsis">{data?.body?.movie?.fullplot}</p>
+            <p class="text-gray-700">{data?.body?.movie?.fullplot}</p>
+            <p class="text-gray-700 pt-4">Genres: {data?.body?.movie?.genres.join(', ')}</p>
+            <a href="/movies" class="text-blue-500 mt-4 block btn">Back to Movies</a>
         </div>
     </div>
 </main>
